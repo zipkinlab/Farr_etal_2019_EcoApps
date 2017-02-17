cda22053c2dec891f8c05bd49298e969df009917
farrmt <matthewtfarr@gmail.com>
2017-02-17 13:03:06 -0500

    Create final script for CDSM

---
+++ DataAnalysis/CDSM.R
@@ -0,0 +1,24 @@
+#---------------------------------------------------------#
+#----Community distance sampling model -------------------#
+#----Script lasted edited by Matthew Farr (2/17/17)-------#
+#---------------------------------------------------------#
+
+#-----------------------#
+#-Set Working Directory-#
+#-----------------------#
+
+setwd("C:/Users/farrm/Documents/GitHub/CDSM/Simulations")
+
+#---------------#
+#-Load Libaries-#
+#---------------#
+
+library(jagsUI)
+
+#-----------#
+#-Load Data-#
+#-----------#
+
+#------------#
+#-BUGS Model-#
+#------------#
