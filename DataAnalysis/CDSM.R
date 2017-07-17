ae41cfbcb890a0085ac5a1b4773736bda57e8f05
farrmt <matthewtfarr@gmail.com>
2017-07-17 16:49:50 -0400

    Clean code, add transect random effect, remove uneeded code

--- DataAnalysis/CDSM.R
+++ DataAnalysis/CDSM.R
@@ -1,7 +1,7 @@
-#---------------------------------------------------------#
-#----Community distance sampling model -------------------#
-#----Script lasted edited by Matthew Farr (3/28/17)-------#
-#---------------------------------------------------------#
+#------------------------------------------------#
+#----Hierarchical Community Distance Sampling----#
+#----Script created by Matthew Farr--------------#
+#------------------------------------------------#
 
 #-----------------------#
 #-Set Working Directory-#
@@ -12,6 +12,7 @@ setwd("C:/Users/farrm/Documents/GitHub/CDSM/DataAnalysis")
 #----------#
 #-Set Seed-#
 #----------#
+
 set.seed(4567)
 
 #---------------#
@@ -36,160 +37,170 @@ attach(DSdata)
 #-BUGS Model-#
 #------------#
 
-sink("CDSM.txt")
+sink("HCDS.txt")
 cat("
     model{
   
-    #Priors
-    
-    #Hyperparameters
-    
+    #-------------#
+    #-HYPERPRIORS-#
+    #-------------#
+
     #Sigma
-    mu_s ~ dnorm(0, 0.01)
-    tau_s <- 1/(sig_s * sig_s)
-    sig_s ~ dunif(0, 500)
+    mu_s ~ dnorm(0, 0.01)        #Mean 
+    tau_s <- 1/(sig_s * sig_s)   #Precision
+    sig_s ~ dunif(0, 500)        #Variance
     
     #Alpha0
-    mu_a0 ~ dnorm(0, 0.01)
-    sig_a0 <- 1/sqrt(tau_a0)
-    tau_a0 ~ dgamma(0.1, 0.1)
+    mu_a0 ~ dnorm(0, 0.01)       #Mean
+    tau_a0 ~ dgamma(0.1, 0.1)    #Precision
+    sig_a0 <- 1/sqrt(tau_a0)     #Variance
+
     
     #Alpha1
-    mu_a1 ~ dnorm(0, 0.01)
-    sig_a1 <- 1/sqrt(tau_a1)
-    tau_a1 ~ dgamma(0.1, 0.1)
+    mu_a1 ~ dnorm(0, 0.01)       #Mean
+    tau_a1 ~ dgamma(0.1, 0.1)    #Precision
+    sig_a1 <- 1/sqrt(tau_a1)     #Variance
+
 
     #Beta1
-    mu_b1 ~ dnorm(0, 0.01)
-    sig_b1 <- 1/sqrt(tau_b1)
-    tau_b1 ~ dgamma(0.1, 0.1)
+    mu_b1 ~ dnorm(0, 0.01)       #Mean
+    tau_b1 ~ dgamma(0.1, 0.1)    #Precision
+    sig_b1 <- 1/sqrt(tau_b1)     #Variance
+
     
-    #Group Size Gamma Parameter
-    
-    r.N ~ dunif(0,100)
-    
-    #Species-specific Parameters
+    #Gamma
+    r.N ~ dunif(0,100)           #Number of groups
+    r.G ~ dunif(0,100)           #Group size
     
     for(s in 1:nspec){
+
+    #Psi
+    tau_p[s] ~ dgamma(0.1, 0.1)  #Precision
+    sig_p[s] <- 1/sqrt(tau_p[s]) #Variance
+
+    #--------#
+    #-PRIORS-#
+    #--------#
     
-    #Abundance parameters
-    alpha0[s] ~ dnorm(mu_a0, tau_a0) #intercept of lambda linear predictor
+    #Detection
+    sigma[s] ~ dnorm(mu_s, tau_s)    #scale parameter
     
-    alpha1[s] ~ dnorm(mu_a1, tau_a1) #coefficient for region covariate
-    
-    #Detection parameter
-    asig[s] ~ dnorm(mu_s, tau_s) #intercept of sigma linear predictor
-    
-    #Group size parameter
-    beta0[s] ~ dunif(0,50) #intercept of lam.gs linear predictor
-    
-    #beta1[s] ~ dnorm(0, 0.01) #coefficient for region covariate
-    beta1[s] ~ dnorm(mu_b1, tau_b1)
-    
-    ##Likelihood
-    
+    #Expected Number of Groups
+    alpha0[s] ~ dnorm(mu_a0, tau_a0) #intercept parameter
+    alpha1[s] ~ dnorm(mu_a1, tau_a1) #effect parameter
+
+    #Expected Group Size
+    beta0[s] ~ dunif(0,50)           #intercept parameter
+    beta1[s] ~ dnorm(mu_b1, tau_b1)  #effect parameter
+
     for(j in 1:nsites){
+
+    #Expected Number of Groups
+    psi[j,s] ~ dnorm(0, tau_p[s])    #transect effect parameter
+
+    #------------#
+    #-LIKELIHOOD-#
+    #------------#
     
     for(t in 1:nreps[j]){
     
-    ## Part 1
-    
-    # construct cell probabilities for nG cells using numerical integration
-    # sum of the area (rectangles) under the detection function
+    #Construct cell probabilities for nG cells using numerical integration
+    #Sum of the area (rectangles) under the detection function
+
     for(k in 1:nD){
     
-    # half normal detection function at midpt (length of rectangle)
+    #Half normal detection function at midpt (length of rectangle)
     g[k,t,j,s] <- exp(-mdpt[k]*mdpt[k]/(2*sigma[s]*sigma[s]))
     
-    # probability of x in each interval (width of rectangle) for both sides of the transect
+    #Proportion of each interval (width of rectangle) for both sides of the transect
     pi[k,t,j,s] <- v/B
     
-    # detection probability for each interval (area of each rectangle)
+    #Detection probability for each distance class k (area of each rectangle)
     f[k,t,j,s] <- g[k,t,j,s] * pi[k,t,j,s]
     
-    # conditional detection probability (scale to 1)
+    #Conditional detection probability (scale to 1)
     fc[k,t,j,s] <- f[k,t,j,s]/pcap[t,j,s]
     
     }#end k loop
     
-    # detection probability at each site (sum of rectangles)
+    #Detection probability at each transect (sum of rectangles)
     pcap[t,j,s] <- sum(f[1:nD,t,j,s])
     
-    ## Part 2
-    
-    #Observed population @ each t,j,s
+    #Observed population @ each t,j,s (N-mixture)
     y[t,j,s] ~ dbin(pcap[t,j,s], N[t,j,s])
     
-    ## Part 3
+    #Latent Number of Groups @ each t,j,s (negative binomial)
+    N[t,j,s] ~ dpois(lambda.star[t,j,s])
     
-    #Population @ each t,j,s
-    N[t,j,s] ~ dpois(lambda[t,j,s])
+    #Expected Number of Groups
+    lambda.star[t,j,s] <- rho[t,j,s] * lambda[t,j,s]
+
+    #Overdispersion parameter for Expected Number of Groups
+    rho[t,j,s] ~ dgamma(r.N, r.N)
+
+    #Linear predictor for Expected Number of Groups
+    lambda[t,j,s] <- exp(alpha0[s] + alpha1[s] * region[j] + psi[j,s] + log(offset[j]))
     
-    #Linear Predictor for Lambda
-    lambda[t,j,s] <- exp(alpha0[s] + alpha1[s] * region[j] + log(offset))
-    
-    ##Group size
-    
-    gs.lam[t,j,s] <- exp(beta0[s] + beta1[s] * region[j] ) 
+    #Expected Group Size
     gs.lam.star[t,j,s] <- gs.lam[t,j,s] * gs.rho[t,j,s]
-    gs.rho[t,j,s] ~ dgamma(r.N, r.N)
-    
+
+    #Overdispersion parameter for Expected Group Size
+    gs.rho[t,j,s] ~ dgamma(r.G, r.G)
+
+    #Linear predictor for Expected Group Size
+    gs.lam[t,j,s] <- exp(beta0[s] + beta1[s] * region[j] + log(offset[j])) 
+
     }#end t loop
 
+    #Mean detection probability @ each j,s
     psite[j,s] <- mean(pcap[1:nreps[j], j, s])
     
     }#end j loop
     
-    #Linear Predictor for Sigma
-    sigma[s] <- exp(asig[s])
-    
-    #Detection probability per species
+    #Mean detection probability for each species
     Dprop[s] <- mean(psite[1:nsites, s])
     
     }#end s loop
 
+    #Mean detection probability for all t,j,s
     TotalDprop <- mean(Dprop[])
-    
-    #Observation i to t,j,s
     
     for(i in 1:nobs){
     
+    #???
     dclass[i] ~ dcat(fc[1:nD, rep[i], site[i], spec[i]])
-    
+
+    #Observed Group Size (zero truncated negative binomial)
     gs[i] ~ dpois(gs.lam.star[rep[i], site[i], spec[i]]) T(1,)
     
     }#end i loop
-    
-    ## Derived quantities
     
     for(s in denspec){
     
     for(j in 1:nsites){
     
     for(t in 1:nreps[j]){
-    
-    #GSrep[t,j,s] <- N[t,j,s] * gs.lam.star[t,j,s]
-    GSrep[t,j,s] <- lambda[t,j,s] * gs.lam.star[t,j,s]
+
+    #Abundance per transect
+    GSrep[t,j,s] <- lambda.star[t,j,s] * gs.lam.star[t,j,s]
     
     }#end t loop
     
+    #Abundance per transect averaged over surveys
     GSsite[j,s] <- mean(GSrep[1:nreps[j], j, s])
+
+    #???
     lambda.N[j,s] <- mean(lambda[1:nreps[j], j, s])
     lambda.G[j,s] <- mean(gs.lam.star[1:nreps[j], j, s])
-    DenGSsite[j,s] <- GSsite[j,s] / area[j]
     
     }#end j loop
     
-    #GS[s] <- sum(GSsite[1:nsites, s])
+    #Mean abundance per transect
     GS[s] <- mean(GSsite[1:nsites, s])   
 
-    DenGS[s] <- mean(DenGSsite[1:nsites, s]) * 100 #per 100 km2
-
-    RegGS[s,1] <- mean(GSsite[1:13, s])
-    RegGS[s,2] <- mean(GSsite[14:17, s])
-    
-    RegDen[s,1] <- mean(DenGSsite[1:13, s]) * 100 #per 100 km2
-    RegDen[s,2] <- mean(DenGSsite[14:17, s]) * 100 #per 100 km2
+    #Abundance per transect for each region
+    RegGS[s,1] <- mean(GSsite[1:13, s])     #Mara Triangle
+    RegGS[s,2] <- mean(GSsite[14:17, s])    #Talek region
     
     }#end s loop
     
@@ -203,7 +214,7 @@ sink()
 
 data <- list(nspec = nspec, nD = nD, v = v, area = area, site = site, rep = rep, spec = spec, 
              y = y, B = B, mdpt = mdpt, nobs = nobs, dclass = dclass, nsites = nsites, 
-             nreps = nreps, gs = gs, region = region, denspec = denspec)
+             nreps = nreps, gs = gs, region = region, denspec = denspec, offset = offset)
 
 #---------------#
 #-Inital values-#
@@ -211,29 +222,27 @@ data <- list(nspec = nspec, nD = nD, v = v, area = area, site = site, rep = rep,
 
 Nst <- y + 1
 
-inits <- function(){list(N = Nst, mu_a0 = runif(1, 0, 1), tau_a0 = runif(1, 0, 1),
-                          mu_s = runif(1, 5, 6), sig_s=runif(1),asig = runif(nspec, 5, 6), 
-                          mu_a1 = runif(1, 0, 1), tau_a1 = runif(1, 0, 1))}
+inits <- function(){list(mu_s = runif(1, 5, 6), sig_s=runif(1), sigma = exp(runif(nspec, 5, 6)), 
+                         mu_a0 = runif(1, 0, 1), tau_a0 = runif(1, 0, 1),
+                         mu_a1 = runif(1, 0, 1), tau_a1 = runif(1, 0, 1),
+                         N = Nst)}
 
 #--------------------#
 #-Parameters to save-#
 #--------------------#
 
-params<-c('mu_a0', 'mu_a1', 'mu_s', 'mu_b1', 'tau_b1', 'tau_a0', 'tau_a1', 'tau_s', 'sigma',
-          'alpha0', 'alpha1', 'beta0', 'beta1', 'GS', 'DenGS', 'RegDen', 
-          'Dprop', 'TotalDprop', 'RegGS')
-
-params<-c('lambda.N', 'lambda.G')
+params <- c('mu_s', 'sig_s', 'mu_a0', 'sig_a0', 'mu_a1', 'sig_a1', 'mu_b1', 'sig_b1', 
+            'r.N', 'r.G', 'sigma_psi', 'alpha0', 'alpha1', 'beta0', 'beta1', 'RegGS')
 
 #---------------#
 #-MCMC settings-#
 #---------------#
 
 nc <- 3
-ni <- 250000
-nb <- 50000
-nt <- 20
+ni <- 150000
+nb <- 100000
+nt <- 10
 
-CDSM <- jags(data = data, inits = inits, parameters.to.save = params, model.file = "CDSM.txt", 
+HCDS <- jags(data = data, inits = inits, parameters.to.save = params, model.file = "HCDS.txt", 
                          n.chains = nc, n.iter = ni, n.burnin = nb, n.thin = nt, store.data = FALSE, parallel = TRUE)
 
